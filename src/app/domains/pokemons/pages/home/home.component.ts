9a4fd46efd606ac2ae4708b0578fcd86132f18d2
Edwin Hernández <stivenh_1224@hotmail.com>
2025-01-06 01:07:26 -0500

    feat(home): import header component to home

--- src/app/domains/pokemons/pages/home/home.component.ts
+++ src/app/domains/pokemons/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Pokemon } from '../../../entities/pokemon.model';
 import { CardComponent } from '../../components/card/card.component';
+import { HeaderComponent } from '../../../shared/components/header/header.component'
 
 @Component({
   selector: 'app-Home',
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
